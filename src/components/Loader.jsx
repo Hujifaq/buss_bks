62180f64c17b39edfe69ac88c18b4c3621adebb2
kadsan2 <sprogram2.kadsan@gmail.com>
2026-08-14 15:48:07 +0700

    finalGram

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,7 +8,7 @@ function Loader({ fullScreen = false }) {
 
   return (
     <div className={containerClasses}>
-      <img src="/src/assets/logo_bks.png" alt='BKS_LOGO' className="w-32 h-auto object-contain" />  
+      <img src="https://apywlcxidcnpbqmectgn.supabase.co/storage/v1/object/sign/Public/logo_bks.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81YTVlNjRlNS1mODdhLTRmMjMtODA1OC1mNTNiYWJmYzk5ODciLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJQdWJsaWMvbG9nb19ia3MucG5nIiwic2NvcGUiOiJkb3dubG9hZCIsImlhdCI6MTc4NjY5NzI2NywiZXhwIjoxODE4MjMzMjY3fQ.1tkzGXAK-jzrfqokXBzQvV03VpKZ8VUpqQT6WQc6xps" alt='BKS_LOGO' className="w-32 h-auto object-contain" />  
       <SyncLoader color="#ff7a00" speedMultiplier={1.2} margin={5} size={15} />
     </div>
   );
